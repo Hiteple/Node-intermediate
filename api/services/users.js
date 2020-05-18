1aea05260aa324be7e84ddd2b1d8a247688e63f9
hiteple <hiteplegold@gmail.com>
2020-05-18 00:39:56 -0300

    Making users schema and users service

---
+++ api/services/users.js
@@ -0,0 +1,26 @@
+const MongoLib = require('../lib/mongo');
+const bcrypt = require('bcrypt');
+
+class UsersService {
+  constructor() {
+    this.collection = 'users';
+    this.mongoDB = new MongoLib();
+  }
+
+  async getUser({ email }) {
+    const [user] = await this.mongoDB.getAll(this.collection, { email });
+    return user;
+  }
+
+  async createUser({ user }) {
+    const { name, email, password } = user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return await this.mongoDB.create(this.collection, {
+      name,
+      email,
+      password: hashedPassword,
+    });
+  }
+}
+
+module.exports = UsersService;
