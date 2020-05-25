692fc3c86e58b58a0a192b319c3d9a902305e093
hiteple <hiteplegold@gmail.com>
2020-05-24 22:39:49 -0300

    Implementing auth in get and post routes

--- ssr-server/config/index.js
+++ ssr-server/config/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-    dev: process.env.NODE_ENV !== 'production',
-    port: process.env.PORT || 3001,
-    apiUrl: process.env.API_URL,
-    apiKeyToken: process.env.API_KEY_TOKEN
-}
+  dev: process.env.NODE_ENV !== "production",
+  port: process.env.PORT || 3001,
+  apiUrl: process.env.API_URL,
+  apiKeyToken: process.env.API_KEY_TOKEN,
+};
 
 module.exports = { config };
--- ssr-server/index.js
+++ ssr-server/index.js
@@ -1,32 +1,85 @@
 const express = require("express");
-
+const passport = require("passport");
+const boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require(axios);
 const { config } = require("./config");
 
 const app = express();
 
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
+
 // body parser
 app.use(express.json());
+app.use(cookieParser());
 
-app.post("/auth/sign-in", async function(req, res, next) {
+// Basic strategy
+require("./utils/auth/strategies/basic");
 
+app.post("/auth/sign-in", async function (req, res, next) {
+  passport.authenticate("basic", function (error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+        });
+
+        return res.status(200).json(user);
+      });
+    } catch (err) {
+      next(err);
+    }
+  })(req, res, next);
 });
 
-app.post("/auth/sign-up", async function(req, res, next) {
+app.post("/auth/sign-in", async function (req, res, next) {
+  // Obtenemos el atributo rememberMe desde el cuerpo del request
+  const { rememberMe } = req.body;
 
+  passport.authenticate("basic", function (error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC,
+        });
+
+        return res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
 });
 
-app.get("/movies", async function(req, res, next) {
+app.get("/movies", async function (req, res, next) {});
 
-});
+app.post("/user-movies", async function (req, res, next) {});
 
-app.post("/user-movies", async function(req, res, next) {
+app.delete("/user-movies/:userMovieId", async function (req, res, next) {});
 
-});
-
-app.delete("/user-movies/:userMovieId", async function(req, res, next) {
-
-});
-
-app.listen(config.port, function() {
-    console.log(`Listening http://localhost:${config.port}`);
+app.listen(config.port, function () {
+  console.log(`Listening http://localhost:${config.port}`);
 });
